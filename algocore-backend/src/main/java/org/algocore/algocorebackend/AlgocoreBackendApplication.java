047e013270d1ec0f4b7593ed3ee6c6f814f033ed
alexelabassi <alex.tamer@yahoo.com>
2025-08-02 21:35:30 +0300

    backend initialisation

---
+++ algocore-backend/src/main/java/org/algocore/algocorebackend/AlgocoreBackendApplication.java
@@ -0,0 +1,13 @@
+package org.algocore.algocorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgocoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgocoreBackendApplication.class, args);
+	}
+
+}
---
+++ algocore-backend/src/test/java/org/algocore/algocorebackend/AlgocoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.algocore.algocorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgocoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
